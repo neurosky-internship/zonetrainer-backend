ccc1b4a939cc845ad9bdee0a640e9af9b4569f9e
cindybohyeon <bohyeoncindy@gmail.com>
2022-11-20 20:33:47 +0900

    feat : users 스키마 생성

---
+++ components/users/schema.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('../../lib/mongo/mongo-handler')();
+const schema = new mongoose.Schema({
+    userId: {type: String, index: 1, required: true},
+    email: {type: String, required: true},
+    name: {type: String, required: true},
+    updatedAt: {type: Date, default: Date.now, index: 1},
+    createdAt: {type: Date, default: Date.now, index: 1}
+  }, {
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model('users', schema, 'users');
